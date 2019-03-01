f71dd699c8e61a4a0db388044fb2b50de2fd14de
jordan.ji-ji@live.fr <jordan.ji-ji@live.fr>
2019-03-01 14:54:17 +0100

    First commit

---
+++ src/main/scala/CheckerActor.scala
@@ -0,0 +1,80 @@
+package upmc.akka.leader
+
+import java.util
+import java.util.Date
+
+import akka.actor._
+import scala.math.Ordering.Implicits._
+
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+abstract class Tick
+case class CheckerTick () extends Tick
+
+class CheckerActor (val id:Int, val terminaux:List[Terminal], electionActor:ActorRef) extends Actor {
+
+     var time : Int = 500
+     val father = context.parent
+
+     var nodesAlive:List[Int] = List()
+     var datesForChecking:List[Date] = List()
+     var lastDate:Date = null
+
+     var leader : Int = -1
+     //JOJO
+     var mapNodeDate:Map[Int, Date] = Map() 
+     //nombre de Beat avant d'etre considerer mort
+     var lifeOrDeadMirror : Int = 3
+
+    def receive = {
+
+         // Initialisation
+        case Start => {
+             self ! CheckerTick
+        }
+
+        // A chaque fois qu'on recoit un Beat : on met a jour la liste des nodes
+        case IsAlive (nodeId) => {
+          mapNodeDate = mapNodeDate + (nodeId -> new Date)
+        }
+
+        case IsAliveLeader (nodeId) => {
+          if(this.leader != nodeId){
+               electionActor ! RebootState
+          }
+          this.leader = nodeId;
+          mapNodeDate = mapNodeDate + (nodeId -> new Date)
+        }
+        // A chaque fois qu'on recoit un CheckerTick : on verifie qui est mort ou pas
+        // Objectif : lancer l'election si le leader est mort
+        case CheckerTick => {
+          context.system.scheduler.scheduleOnce(new FiniteDuration(time, MILLISECONDS), self ,CheckerTick)
+
+          //Ensemble des noeud mort
+          var setDead = mapNodeDate.filter( {
+               case (key, date) => Math.abs(date.getTime() - lastDate.getTime()) >=  lifeOrDeadMirror * time 
+          }).keys
+
+          //Delete les noeud mort de la map
+          mapNodeDate --= setDead
+
+          var leadDead =false
+
+          setDead.foreach({
+               key => if(key == leader)leadDead=true; else this.father ! Message("Node "+key+ " is dead")
+          })
+
+          if(leadDead){
+               leader = -1
+               father ! Message("LEADER is dead => ELECTION")
+               electionActor ! StartWithNodeList(mapNodeDate.keys.toList)
+          }
+
+          lastDate = new Date
+        }
+
+    }
+
+
+}
---
+++ src/main/scala/ElectionActor.scala
@@ -0,0 +1,183 @@
+package upmc.akka.leader
+
+import akka.actor._
+
+abstract class NodeStatus
+case class Passive () extends NodeStatus
+case class Candidate () extends NodeStatus
+case class Dummy () extends NodeStatus
+case class Waiting () extends NodeStatus
+case class Leader () extends NodeStatus
+
+abstract class LeaderAlgoMessage
+case class Initiate () extends LeaderAlgoMessage
+case class ALG (list:List[Int], nodeId:Int) extends LeaderAlgoMessage
+case class AVS (list:List[Int], nodeId:Int) extends LeaderAlgoMessage
+case class AVSRSP (list:List[Int], nodeId:Int) extends LeaderAlgoMessage
+case class RebootState () extends LeaderAlgoMessage
+
+case class StartWithNodeList (list:List[Int])
+
+class ElectionActor (val id:Int, val terminaux:List[Terminal]) extends Actor {
+
+     val father = context.parent
+     var nodesAlive:List[Int] = List(id)
+
+     var candSucc:Int = -1
+     var candPred:Int = -1
+     var status:NodeStatus = new Passive ()
+
+     def receive = {
+
+          // Initialisation
+          case Start => {
+               self ! Initiate
+          }
+
+          case RebootState => {
+               println("REBOOT !")
+               this.status = new Passive()
+               this.candPred = -1
+               this.candSucc = -1
+               this.nodesAlive = List(id)
+          }
+
+          case StartWithNodeList (list) => {
+               if (list.isEmpty) {
+                    this.nodesAlive = this.nodesAlive:::List(id)
+               }
+               else {
+                    this.nodesAlive = list ::: List(id)
+               }
+               this.nodesAlive = this.nodesAlive.distinct
+               this.nodesAlive = this.nodesAlive.sorted
+               println(this.nodesAlive)
+               // Debut de l'algorithme d'election
+               self ! Initiate
+          }
+
+          case Initiate => {
+               if(status == Passive() ){
+                    status = Candidate()
+                    this.candPred = -1
+                    this.candSucc = -1
+                    println("SEND ALG "+ id + " -> succ de "+ id) 
+                    getRemoteRingSuccessor(id, nodesAlive) ! ALG(nodesAlive, id)
+               }
+          }
+
+          case ALG (list, init) => {
+               println("REC ALG "+init+" "+status+" candSucc="+ candSucc +" candPred=" +candPred)
+               status match {
+                    case Passive() => {
+                         status = Dummy()
+                         println("SEND ALG "+ init + " -> succ de "+ id) 
+                         getRemoteRingSuccessor(id, list) ! ALG(list, init)
+                    }
+                    case Candidate() => {
+                         this.candPred = init
+                         if(id > init){
+                              if(this.candSucc == -1){
+                                   status = Waiting()
+                                   println("SEND AVS "+ id + " -> "+ init)
+                                   getRemoteById(init) ! AVS(list, id)
+                              } else{
+                                   println("SEND AVSRP "+ candPred + " -> "+ candSucc)
+                                   getRemoteById(candSucc) ! AVSRSP(list, candPred)
+                                   status = Dummy()
+                              }
+                         }
+                         if(init == id){
+                              status = Leader()
+                              father ! LeaderChanged(id)
+                              broadcastLeadChange(id)                    
+                         }
+                    }
+
+                    case _ =>
+               }
+          }
+
+          case AVS (list, j) => {
+               println("REC AVS("+j+") "+status+" candSucc="+ candSucc +" candPred=" +candPred)
+               
+               status match {
+                    case Candidate() => {
+                         if(this.candPred == -1){
+                              this.candSucc = j
+                         } else{
+                              println("SEND AVSRP "+ this.candPred + " -> "+ j)
+                              getRemoteById(j) ! AVSRSP(list, this.candPred)
+                              status = Dummy()
+                         }
+                    }
+                    case Waiting() => {
+                         this.candSucc = j
+                         println("Waiting candSucc="+ j)
+                    }
+
+                    case _ =>
+               }
+          }
+
+          case AVSRSP (list, k) => {
+               println("REC AVSRP "+k+" "+status+" candSucc="+ candSucc +" candPred=" +candPred)
+               status match {
+                    case Waiting() =>{
+                         if(id == k){
+                              status = Leader()
+                              this.father ! LeaderChanged(id)
+                              broadcastLeadChange(id)
+                         }
+                         else {
+                              candPred = k
+                              if(candSucc == -1){
+                                   if(k < id){
+                                        status = Waiting()
+                                        println("SEND AVSRP "+ id + " -> "+ k)
+                                        getRemoteById(k) ! AVS(list, id)
+                                   }
+                              }
+                              else{ 
+                                   status = Dummy()
+                                   println("SEND AVSRP "+ k + " -> "+ candSucc)
+                                   getRemoteById(candSucc) ! AVSRSP(list, k)
+                              }
+                         }
+                    }
+                    case _ =>
+               }
+          }
+
+     }
+
+     def getRemoteRingSuccessor(i:Int, list: List[Int]) : ActorSelection = {
+          getRemoteById(ringSuccessor(i, list))   
+     }
+
+     def broadcastLeadChange(i:Int){
+          terminaux.foreach({
+               case n => {
+                    if(n.id != id ){
+                         val remote = context.actorSelection("akka.tcp://LeaderSystem" + n.id + "@" + n.ip + ":" + n.port + "/user/Node")
+                         remote ! LeaderChanged(i)
+                    }
+               }
+          })
+     }
+
+     //TODO unsafe en cas de deconnexion d'un actor
+     def getRemoteById(i:Int) : ActorSelection = {
+          val n = terminaux.filter({
+               case (term) => term.id == i 
+          })(0)   // <==  
+          context.actorSelection("akka.tcp://LeaderSystem" + n.id + "@" + n.ip + ":" + n.port + "/user/Node/electionActor")
+     }
+
+     def ringSuccessor(i:Int, list: List[Int]) : Int = {
+          list( (list.indexOf(i)+1) % (list.size))
+     }
+
+
+
+}
